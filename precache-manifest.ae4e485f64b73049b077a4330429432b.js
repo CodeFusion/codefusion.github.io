73a6290caa14368e85d29ab9e06c9d9c8f4cab7e
Kyle Ramey <codefusion1@gmail.com>
2022-12-13 20:23:40 -0400

    Public test version

---
+++ precache-manifest.ae4e485f64b73049b077a4330429432b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16df297faefe2dab95f9dea4e22454e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2a1e3d3e2e345f5f7a2",
+    "url": "/static/css/main.c65623f0.chunk.css"
+  },
+  {
+    "revision": "264b1b8ce36f27738f0e",
+    "url": "/static/js/2.d12bc932.chunk.js"
+  },
+  {
+    "revision": "e2a1e3d3e2e345f5f7a2",
+    "url": "/static/js/main.60ccb124.chunk.js"
+  },
+  {
+    "revision": "05b83fcaa5eb0e0cc5f8",
+    "url": "/static/js/runtime~main.e11d9638.js"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "33211a17eb64aaa6b94254b72a70bcb9",
+    "url": "/static/media/smile.33211a17.svg"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "/static/media/view-info.415b027d.ico"
+  }
+]);
